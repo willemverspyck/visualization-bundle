c30393bbd4d1b13a4dcd709f845594742092b25b
willemverspyck <willemverspyck@users.noreply.github.com>
2023-12-11 09:27:38 +0100

    Add assert method

---
+++ src/Utility/DataUtility.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spyck\VisualizationBundle\Utility;
+
+use Exception;
+
+final class DataUtility
+{
+    /**
+     * @throws Exception
+     */
+    public static function assert(bool $condition, Exception $exception = null): void
+    {
+        if ($condition) {
+            return;
+        }
+
+        if (null === $exception) {
+            throw new Exception();
+        }
+
+        throw $exception;
+    }
+}
