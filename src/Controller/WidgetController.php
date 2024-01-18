fcfbe028e234f67e89401b4de1f29130db5d49b8
willemverspyck <willemverspyck@users.noreply.github.com>
2024-01-18 11:13:15 +0100

    Remove duplicate method

--- src/Controller/WidgetController.php
+++ src/Controller/WidgetController.php
@@ -27,46 +27,27 @@ final class WidgetController extends AbstractController
      * @throws Exception
      * @throws InvalidArgumentException
      */
-    #[Route(path: '/api/widget/{widgetId}.{_format}', name: 'spyck_visualization_widget_show', requirements: ['widgetId' => Requirement::DIGITS], methods: [Request::METHOD_GET])]
-    public function show(Request $request, ViewService $viewService, WidgetService $widgetService, int $widgetId): Response
+    #[Route(path: '/api/widget/{widgetId}.{_format}', name: 'spyck_visualization_widget_item', requirements: ['widgetId' => Requirement::DIGITS], methods: [Request::METHOD_GET])]
+    public function item(Request $request, ViewService $viewService, WidgetService $widgetService, int $widgetId): Response
     {
-        set_time_limit(120);
-
         try {
             $variables = $request->query->all();
 
             $widget = $widgetService->getWidgetDataById($widgetId, $variables);
 
-            $viewInstance = $viewService->getView($request->getRequestFormat());
+            $view = $viewService->getView($request->getRequestFormat());
 
-            $content = $viewInstance->getContent($widget);
+            $content = $view->getContent($widget);
 
             $headers = [];
 
-            if (null !== $viewInstance->getExtension()) {
-                $headers['Content-Disposition'] = sprintf('attachment; filename="%s.%s"', $viewInstance->getFile($widget->getName(), $widget->getParametersAsStringForSlug()), $viewInstance->getExtension());
+            if (null !== $view->getExtension()) {
+                $headers['Content-Disposition'] = sprintf('attachment; filename="%s.%s"', $view->getFile($widget->getName(), $widget->getParametersAsStringForSlug()), $view->getExtension());
             }
 
             return new Response($content, Response::HTTP_OK, $headers);
         } catch (ParameterException $parameterException) {
             throw $this->createNotFoundException($parameterException->getMessage());
         }
-    }
-
-    /**
-     * @throws Exception
-     * @throws InvalidArgumentException
-     */
-    #[Cache(expires: '+2 hour', maxage: '7200', smaxage: '7200', public: true)]
-    #[Route(path: '/api/widget/{widgetId}', name: 'spyck_visualization_widget_item', requirements: ['widgetId' => Requirement::DIGITS], methods: [Request::METHOD_GET])]
-    public function item(Request $request, ResponseService $responseService, WidgetService $widgetService, int $widgetId): Response
-    {
-        $variables = $request->query->all();
-
-        $block = $widgetService->getWidgetDataById($widgetId, $variables)->getBlocks()->first();
-
-        $widget = $block instanceof Block ? $block->getWidget() : null;
-
-        return $responseService->getResponseForItem(data: $widget, groups: ['spyck:visualization:widget:item']);
     }
 }
