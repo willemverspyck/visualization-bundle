c3026f96832e5409d325bd72b86c9d6e67dc7875
willemverspyck <willemverspyck@users.noreply.github.com>
2024-08-13 11:49:40 +0200

    Semicolon separated numbers must be with comma

--- src/View/SsvView.php
+++ src/View/SsvView.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Spyck\VisualizationBundle\View;
 
+use DateTimeInterface;
+use Spyck\VisualizationBundle\Config\Config;
+use Spyck\VisualizationBundle\Field\FieldInterface;
+
 final class SsvView extends CsvView
 {
     public static function getName(): string
@@ -14,5 +18,17 @@ final class SsvView extends CsvView
     protected function getSeparator(): string
     {
         return ';';
+    }
+
+    protected function getValue(string $type, Config $config, array|bool|DateTimeInterface|float|int|string|null $value): bool|float|int|string|null
+    {
+        if (null === $value) {
+            return null;
+        }
+
+        return match ($type) {
+            FieldInterface::TYPE_CURRENCY, FieldInterface::TYPE_NUMBER, FieldInterface::TYPE_PERCENTAGE => str_replace('.', ',', (string) $value),
+            default => parent::getValue($type, $config, $value),
+        };
     }
 }
