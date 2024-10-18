0d8920ff40846ef784e01acf58f4477fb8a17f78
Willem Verspyck <willemverspyck@users.noreply.github.com>
2024-10-18 14:17:32 +0200

    Fix repository

--- src/Repository/ScheduleRepository.php
+++ src/Repository/ScheduleRepository.php
@@ -5,19 +5,20 @@ declare(strict_types=1);
 namespace Spyck\VisualizationBundle\Repository;
 
 use Doctrine\Persistence\ManagerRegistry;
+use Spyck\VisualizationBundle\Entity\AbstractSchedule;
 use Spyck\VisualizationBundle\Entity\ScheduleInterface;
 
 class ScheduleRepository extends AbstractRepository
 {
     public function __construct(ManagerRegistry $managerRegistry)
     {
-        parent::__construct($managerRegistry, Schedule::class);
+        parent::__construct($managerRegistry, AbstractSchedule::class);
     }
 
     public function getScheduleByCode(string $discriminator, string $code): ?ScheduleInterface
     {
         return $this->createQueryBuilder('schedule')
-            ->where('schedule INSTANCE OF :distriminator')
+            ->where('schedule INSTANCE OF :discriminator')
             ->andWhere('schedule.code = :code')
             ->andWhere('schedule.active = TRUE')
             ->setParameter('discriminator', $discriminator)
@@ -32,7 +33,7 @@ class ScheduleRepository extends AbstractRepository
     public function getSchedules(string $discriminator): array
     {
         return $this->createQueryBuilder('schedule')
-            ->where('schedule INSTANCE OF :distriminator')
+            ->where('schedule INSTANCE OF :discriminator')
             ->andWhere('schedule.active = TRUE')
             ->setParameter('discriminator', $discriminator)
             ->getQuery()
