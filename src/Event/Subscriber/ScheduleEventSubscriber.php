806338e4653d4c25c9904739236a97d648c7aab2
willemverspyck <willemverspyck@users.noreply.github.com>
2024-12-31 14:42:50 +0100

    Allow to use "Last day of the Month"

--- src/Event/Subscriber/ScheduleEventSubscriber.php
+++ src/Event/Subscriber/ScheduleEventSubscriber.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Spyck\VisualizationBundle\Event\Subscriber;
 
+use DateTime;
+use DateTimeInterface;
+use Spyck\VisualizationBundle\Entity\ScheduleInterface;
 use Spyck\VisualizationBundle\Event\ScheduleEvent;
 use Spyck\VisualizationBundle\Service\MailService;
 use Spyck\VisualizationBundle\Service\PreloadService;
@@ -28,31 +31,75 @@ final class ScheduleEventSubscriber implements EventSubscriberInterface
 
     public function onSchedule(ScheduleEvent $event): void
     {
-        $date = $event->getDate();
         $schedule = $event->getSchedule();
+        $date = $event->getDate();
 
-        if (false === $this->match($schedule->getMatchHours(), (int) $date->format('G'))) {
+        dump($schedule->getName());
+        dump($date->format('Y-m-d H:i:s'));
+
+        if (false === $this->isMatchBySchedule($schedule, $date)) {
+            dump('False');
+
             return;
         }
 
-        if (false === $this->match($schedule->getMatchDays(), (int) $date->format('j'))) {
-            return;
-        }
-
-        if (false === $this->match($schedule->getMatchWeeks(), (int) $date->format('W'))) {
-            return;
-        }
-
-        if (false === $this->match($schedule->getMatchWeekdays(), (int) $date->format('N'))) {
-            return;
-        }
+        dump('True');
 
         $this->mailService->executeMailMessageBySchedule($schedule);
         $this->preloadService->executePreloadMessageBySchedule($schedule);
     }
 
-    private function match(array $data, int $value): bool
+    private function isMatch(array $data, array $matches): bool
     {
-        return 0 === count($data) || in_array($value, $data, true);
+        if (0 === count($data)) {
+            return true;
+        }
+
+        foreach ($matches as $match) {
+            if (in_array($match, $data, true)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function isMatchBySchedule(ScheduleInterface $schedule, DateTimeInterface $date): bool
+    {
+        if (false === $this->isMatch($schedule->getMatchHours(), $this->getMatch($date, 'G'))) {
+            return false;
+        }
+
+        if (false === $this->isMatch($schedule->getMatchDays(), $this->getMatch($date, 'j'))) {
+            return false;
+        }
+
+        if (false === $this->isMatch($schedule->getMatchWeeks(), $this->getMatch($date, 'W'))) {
+            return false;
+        }
+
+        if (false === $this->isMatch($schedule->getMatchWeekdays(), $this->getMatch($date, 'N'))) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function getMatch(DateTimeInterface $date, string $format): array
+    {
+        $matches = [
+            (int) $date->format($format),
+        ];
+
+        if ('j' === $format) {
+            $dateLastDay = DateTime::createFromInterface($date);
+            $dateLastDay->modify('Last day of this month');
+
+            if ($date->format('j') === $dateLastDay->format('j')) {
+                $matches[] = 'L';
+            }
+        }
+
+        return $matches;
     }
 }
