4bc7b2be10b3dd89a8b06defed503cc86ecaa105
willemverspyck <willemverspyck@users.noreply.github.com>
2025-01-03 09:56:48 +0100

    Remove not used function

--- src/Repository/AbstractRepository.php
+++ src/Repository/AbstractRepository.php
@@ -28,26 +28,4 @@ abstract class AbstractRepository extends ServiceEntityRepository
 
         return $user;
     }
-
-    /**
-     * Truncate field if the length is too long.
-     */
-    protected function truncate(string $className, string $field, ?string $value): ?string
-    {
-        if (null === $value) {
-            return null;
-        }
-
-        $metaData = $this->getEntityManager()->getClassMetadata($className);
-
-        if (array_key_exists($field, $metaData->fieldMappings)) {
-            $length = $metaData->fieldMappings[$field]['length'];
-
-            if (mb_strlen($value) > $length) {
-                $value = mb_substr($value, 0, $length);
-            }
-        }
-
-        return $value;
-    }
 }
