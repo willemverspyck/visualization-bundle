57ab7210da2a466e1bdcc96f49efb144c38206c9
Willem Verspyck <willemverspyck@users.noreply.github.com>
2025-05-19 13:28:54 +0200

    Remove duplicate code

--- src/Utility/DateTimeUtility.php
+++ src/Utility/DateTimeUtility.php
@@ -16,45 +16,31 @@ final class DateTimeUtility
     /**
      * @throws Exception
      */
-    public static function getDateFromString(?string $content, string $format = self::FORMAT_DATE): ?DateTimeImmutable
+    public static function getDateFromString(?string $content): ?DateTimeImmutable
     {
-        if (null === $content) {
-            return null;
-        }
-
-        $date = DateTimeImmutable::createFromFormat($format, $content);
-
-        if (false === $date) {
-            throw new Exception(sprintf('Date value "%s" not valid', $content));
-        }
-
-        $date->setTime(0, 0);
-
-        return $date;
+        return self::getFromString($content, self::FORMAT_DATE)?->setTime(0, 0);
     }
 
     /**
      * @throws Exception
      */
-    public static function getDateTimeFromString(?string $content, string $format = self::FORMAT_DATETIME): ?DateTimeImmutable
+    public static function getDateTimeFromString(?string $content): ?DateTimeImmutable
     {
-        if (null === $content) {
-            return null;
-        }
-
-        $date = DateTimeImmutable::createFromFormat($format, $content);
-
-        if (false === $date) {
-            throw new Exception(sprintf('DateTime value "%s" not valid', $content));
-        }
-
-        return $date;
+        return self::getFromString($content, self::FORMAT_DATETIME);
     }
 
     /**
      * @throws Exception
      */
-    public static function getTimeFromString(?string $content, string $format = self::FORMAT_TIME): ?DateTimeImmutable
+    public static function getTimeFromString(?string $content): ?DateTimeImmutable
+    {
+        return self::getFromString($content, self::FORMAT_TIME);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public static function getFromString(?string $content, string $format): ?DateTimeImmutable
     {
         if (null === $content) {
             return null;
@@ -63,7 +49,7 @@ final class DateTimeUtility
         $date = DateTimeImmutable::createFromFormat($format, $content);
 
         if (false === $date) {
-            throw new Exception(sprintf('Time value "%s" not valid', $content));
+            throw new Exception(sprintf('Date "%s" does not match "%s"', $content, $format));
         }
 
         return $date;
