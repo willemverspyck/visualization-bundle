274a7a44869b61af0b50ecf9e6fde9ee83b0f469
Willem Verspyck <willemverspyck@users.noreply.github.com>
2025-11-18 13:32:58 +0100

    Update config of PHP CS Fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -12,11 +12,7 @@ $finder = (new PhpCsFixer\Finder())
 return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
-        'array_syntax' => [
-            'syntax' => 'short',
-        ],
         'global_namespace_import' => [
-            'import_classes' => true,
             'import_constants' => true,
             'import_functions' => true,
         ],
